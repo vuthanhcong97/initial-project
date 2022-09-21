c5afac3867a6a2c847066bf8978ab3a47901668b
universuccess <cong@wee.vn>
2022-09-21 11:04:26 +0700

    update config redux devtool

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,25 +1,31 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistState } from '@redux-devtools/core';
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-
 import createReducer from './reducer'
-
-const composeEnhancers = composeWithDevTools({
-	trace: true,
-	traceLimit: 25,
-});
+import DevTools from "../components/DevTools";
 
 const middleware = [thunk]
+
+const enhancer = compose(
+	applyMiddleware(...middleware),
+	// Required! Enable Redux DevTools with the monitors you chose
+	DevTools.instrument(),
+	// Optional. Lets you write ?debug_session=<key> in address bar to persist debug sessions
+	persistState(getDebugSessionKey())
+);
+
+function getDebugSessionKey() {
+	// You can write custom logic here!
+	// By default we try to read the key from ?debug_session=<key> in the address bar
+	const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
+	return matches && matches.length > 0 ? matches[1] : null;
+}
 
 const initialState = {}
 
 const initialReducer = createReducer()
 
-const store =  createStore(
-	initialReducer,
-	initialState,
-	composeEnhancers(applyMiddleware(...middleware))
-)
+const store =  createStore(initialReducer, initialState, enhancer)
 
 store.asyncReducers = {};
 store.injectReducer = (key, reducer) => {
