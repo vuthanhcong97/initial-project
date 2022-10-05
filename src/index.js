56111262cef5fe3ca3764c5e423b12cb8c435fa8
universuccess <vuthanhcong97@gmail.com>
2022-10-05 13:57:34 +0700

    update use redux toolkit instead of redux origin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-if (process.env.NODE_ENV === "production") {
-	module.exports = require("./index.prod")
-} else {
-	module.exports = require("./index.dev")
-}
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter as Router } from "react-router-dom"
+import reportWebVitals from "./reportWebVitals"
+
+// components
+import App from "./App"
+
+// redux
+import store from "./redux"
+import { Provider } from "react-redux"
+
+// styles
+import { GlobalStyles } from "./GlobalStyles"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(
+	<Provider store={store}>
+		<Router>
+			<React.StrictMode>
+				<GlobalStyles />
+				<App />
+			</React.StrictMode>
+		</Router>
+	</Provider>
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,5 +1,26 @@
-if (process.env.NODE_ENV === "production") {
-	module.exports = require("./index.prod")
-} else {
-	module.exports = require("./index.dev")
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit"
+import thunk from "redux-thunk"
+import createReducer from "./reducer"
+
+const middleware = [thunk]
+
+const enhancer = applyMiddleware(...middleware)
+
+const initialState = {}
+
+const initialReducer = createReducer()
+
+const store = configureStore({
+	reducer: initialReducer,
+	preloadedState: initialState,
+	enhancers: enhancer,
+})
+
+store.asyncReducers = {}
+store.injectReducer = (key, reducer) => {
+	store.asyncReducers[key] = reducer
+	store.replaceReducer(createReducer(store.asyncReducers))
+	return store
 }
+
+export default store
