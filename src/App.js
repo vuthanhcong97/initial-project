f676098522f4eed30bae4534690e54d0c8a149ba
universuccess <vuthanhcong97@gmail.com>
2023-03-09 15:21:32 +0700

    update config route

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
 						/>
 					)
 				})}
-				<Route path="*" element={<Navigate to="/" />} />
+				<Route path="*" element={<Navigate to="/" replace />} />
 			</Routes>
 		</WrapApp>
 	)
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -3,13 +3,11 @@ import { lazy } from "react"
 const routes = [
 	{
 		path: "/",
-		exact: true,
 		public: true,
 		component: lazy(() => import("./Home")),
 	},
 	{
 		path: "/contact",
-		exact: true,
 		public: true,
 		component: lazy(() => import("./Contact")),
 	},
@@ -18,7 +16,6 @@ const routes = [
 process.env.NODE_ENV === "development" &&
 	routes.push({
 		path: "/all-image",
-		exact: false,
 		public: true,
 		component: lazy(() => import("./AllImage")),
 	})
